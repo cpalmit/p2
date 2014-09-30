f2a2549b25e4f01c83cfc6efd35fe3ca543a47b0
CP <cpalmit@users.noreply.github.com>
2014-09-29 23:12:38 -0400

    restrict count to 1-9

--- logic.php
+++ logic.php
@@ -7,8 +7,6 @@
 		return rtrim($array[rand(0,$max)]);	
 	}
 	
-
-
 	// Figure out what options were selected
 	if(isset($_POST["uppercase"])) {
 		$uppercase= true;
@@ -28,42 +26,46 @@
 		$number = false;
 	}
 	
+	//count has to be set to continue
 	if(isset($_POST["count"])) {
 		$count = $_POST["count"];
-	} 
-
-	//array to hold password words
-	$selectedWords = [];   
+		if(($count > 9) || ($count < 1)){
+			$password =  "Hey, stop trying to hack my form!"; //only 1-9 numbers allowed
+		} else {
+		
+			//array to hold password words
+			$selectedWords = [];   
 	
-	// randomly select words from words.txt	
-	if ($wordlist= file("words.txt")){ 
-		for ($i=0; $i < $count; $i++){
-			//array_push($selectedWords,$word);
-			$selectedWords[$i]=getRandom($wordlist);  
+			// randomly select words from words.txt	
+			if($wordlist= file("words.txt")){ 
+				for ($i=0; $i < $count; $i++){
+					//array_push($selectedWords,$word);
+					$selectedWords[$i]=getRandom($wordlist);  
+				}
+			}
+
+			// convert first letter to uppercase
+			if($uppercase){
+				foreach($selectedWords as $i => $word){
+					$selectedWords[$i]= ucfirst($word);
+				}
+			}
+	
+			// add a random symbol to end of first word
+			$symbols=["@","#","%","&","!","*","$"];
+			if($symbol){
+				$selectedSymbol = getRandom($symbols);  //get a random symbol
+				//$symWord = getRandom($selectedWords).$selectedSymbol; // add symbol to random selected word
+				$selectedWords[0]=$selectedWords[0].$selectedSymbol;
+			}
+	
+			// add a random number from 1-100 to end of first word
+			if($number){
+				$selectedNumber=rand(0,100);  //random number between 0-100
+				$selectedWords[0]=$selectedWords[0].$selectedNumber;
+			}
+	
+			$password = implode("-", $selectedWords);  //the final password
 		}
 	}
-
-	// convert first letter to uppercase
-	if ($uppercase){
-		foreach($selectedWords as $i => $word){
-			$selectedWords[$i]= ucfirst($word);
-		}
-	}
-	
-	// add a random symbol to end of first word
-	$symbols=["@","#","%","&","!","*","$"];
-	if ($symbol){
-		$selectedSymbol = getRandom($symbols);  //get a random symbol
-		//$symWord = getRandom($selectedWords).$selectedSymbol; // add symbol to random selected word
-		$selectedWords[0]=$selectedWords[0].$selectedSymbol;
-	}
-	
-	// add a random number from 1-100 to end of first word
-	if ($number){
-		$selectedNumber=rand(0,100);  //random number between 0-100
-		$selectedWords[0]=$selectedWords[0].$selectedNumber;
-	}
-	
-$password = implode("-", $selectedWords);  //the final password
-	
 ?>
